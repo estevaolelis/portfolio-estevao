38974c77a8a317bf1720357dd34012600d245d3d
Estêvão Lélis <estevaolelis@gmail.com>
2026-02-20 12:11:19 -0300

    feat: coloquei a foto e ajustei o padding e o margin

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+
 export default function About() {
   return (
     <header className="w-full min-h-screen">
-      <div className="mx-auto max-w-xl px-6 pt-8">
+      <div className="mx-auto max-w-xl px-1 pt-8">
         <h1 className="text-white text-2xl font-bold">About me</h1>
+
+        <div className="text-center space-y-6 mb-6 animate-fade-in">
+          <div className="w-24 h-24 md:w-48 md:h-48 rounded-full overflow-hidden mx-auto border-4 border-zinc-800 shadow-2xl">
+             <Image
+                src="/eu.jpg"
+                alt="Estevão Lelis"
+                width={800}
+                height={500}
+              />
+          </div>
+        </div>
 
         <div className="space-y-6 text-m text-gray-300 leading-relaxed">
           <p>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { CenterUnderline } from "@/components/ui/underline-animation";
 export default function NavBar() {
   return (
     <header>
-      <div className="mx-auto max-w-xl px-6 pt-8">
+      <div className="mx-auto max-w-xl px-1 pt-8">
         <nav className="flex h-20 items-center justify-between mt-12">
           <a className="text-3xl font-bold text-white cursor-pointer">Estêvão Lélis</a>
           <div className="flex items-center gap-5">
